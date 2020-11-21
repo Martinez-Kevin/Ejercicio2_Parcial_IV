0652a1fe724cbd66a0de9de35036fc675596a5bf
Martinez-Kevin <70768699+Martinez-Kevin@users.noreply.github.com>
2020-11-21 00:13:12 -0600

    Agregar archivos de proyecto.

---
+++ Compra.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_Parcial_IV
+{
+    public delegate bool compara(decimal dato);
+    class Compra
+    { 
+        public decimal monto_compra { get; set; }
+
+        public decimal descuento { get; set; }
+
+        public decimal desc{ get; set; }
+        public decimal monto_total { get; set; }
+
+
+        public void ImprimirCondicion(compara comp, decimal desc)
+        {
+            if (comp(monto_compra))
+            {
+                
+                descuento = monto_compra * desc;
+                monto_total = monto_compra - descuento;
+                Console.WriteLine("Monto de la compra: {0},   Descuento:{1}", monto_compra, descuento, "\n");
+                Console.WriteLine("\nMonto total a pagar: {0} ", monto_total);
+            }
+        }
+    }
+
+   
+   
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_Parcial_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compra cmp = new Compra();
+
+            Console.Write("Ingrese el monto de la compra: ");
+            cmp.monto_compra = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Monto de la compra y descuento\n");
+
+            
+            cmp.ImprimirCondicion((monto) => monto >= 10000 &&  monto <= 20000, cmp.desc=0.10m);
+
+            cmp.ImprimirCondicion((monto) => monto >= 20001 && monto <= 50000, cmp.desc = 0.30m);
+
+            cmp.ImprimirCondicion((monto) => monto > 50000, cmp.desc = 0.50m);
+
+            Console.ReadKey();
+        }
+    }
+}
